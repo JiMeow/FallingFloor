afb1a41859f56de6bd23f7c495b84c064fd44dae
jiratQ <jirat123@icloud.com>
2022-08-25 13:42:05 +0700

    change back to android

--- Assets/EndGameController.cs
+++ Assets/EndGameController.cs
@@ -34,11 +34,11 @@ public class EndGameController : MonoBehaviour
             deathCountController.IncreaseDeathCount();
         }
         Time.timeScale = 1;
-        // AdManager.instance.PlayNormalAd(() =>
-        // {
-        //     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        // });
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        AdManager.instance.PlayNormalAd(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // public void RestartWithAd()
